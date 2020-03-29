85a0e14b0b6aad10925429438b02ba6557a9c00b
luxjoshyua <josh.e.fielding@gmail.com>
2020-03-30 08:47:53 +1100

    added tutorial part four - graphql

---
+++ tute-part-four/src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+    <Layout>
+        {/* access the site title using a query */}
+        <h1>About {data.site.siteMetadata.title}</h1>
+        <p>
+            We're the only site running on your computer dedicated to showing the best
+            photos and videos of pandas eating lots of food. 
+        </p>
+    </Layout>
+)
+
+// Page queries live outside of the component definition - by convention at the end of a page component file - 
+// and are only available on page components
+
+export const query = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
